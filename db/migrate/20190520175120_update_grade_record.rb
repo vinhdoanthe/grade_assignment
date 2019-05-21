e47689f5befd64aa92ab95e3b8aa58492e1808f7
Vinh Doan-The <vinhdt@funix.edu.vn>
2019-05-21 20:24:49 +0700

    Update migration files (go to the past)

--- db/migrate/20190520175120_update_grade_record.rb
+++ db/migrate/20190520175120_update_grade_record.rb
@@ -1,6 +1,6 @@
 class UpdateGradeRecord < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :grade_records, :mentor_id
+    remove_reference :grade_records, :mentor
     remove_column :grade_records, :mentor_id
     add_column :grade_records, :mentor_email, :string
   end
